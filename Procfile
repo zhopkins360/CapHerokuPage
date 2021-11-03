4e142266611c4012e2e3ba69fd944be6483c562e
ZackHBigComputer <zhopkins@mail.bradley.edu>
2021-11-03 10:47:50 -0500

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python webScript.py
--- webScript.py
+++ webScript.py
@@ -22,4 +22,4 @@ def result():
     return render_template("page.html", isSol = isSol)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=(process.env.PORT or 5000))
