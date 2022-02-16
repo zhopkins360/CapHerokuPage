e87d118e6d50efd6032c9b426d4a0d699387093b
Zackary Hopkins <zhopkins@mail.bradley.edu>
2022-02-16 12:07:45 -0600

    Bug Fixes

--- webScript.py
+++ webScript.py
@@ -61,5 +61,5 @@ def uploadmatrix():
     return render_template("uploadmatrix.html",message="Upload")
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT',33507))
-    app.run(debug=True)
+    #port = int(os.environ.get('PORT',5000))
+    #app.run(debug=True)
